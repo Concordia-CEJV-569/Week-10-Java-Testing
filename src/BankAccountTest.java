de15db91d9904a967b1a17e889bcb7cedb9f6d9f
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-11-17 15:25:39 -0500

    Assertions with Junit

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -1,26 +1,39 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static junit.framework.TestCase.*;
 
 class BankAccountTest {
 
     @Test
-    void deposit() {
+    public void deposit() throws Exception {
+        BankAccount account = new BankAccount("Logan", "Paul", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+        // assertEquals(1200.00, account.getBalance(), 0); BAD PRACTICE
+    }
+
+    @Test
+    public void withdraw() throws Exception {
         fail("This test has yet to be implemented");
     }
 
     @Test
-    void withdraw() {
-        fail("This test has yet to be implemented");
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Logan", "Paul", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
 
     @Test
-    void getBalance() {
-        fail("This test has yet to be implemented");
+    public void getBalance_withdraw() throws Exception {
+        BankAccount account = new BankAccount("Logan", "Paul", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
     }
 
     @Test
-    void dummyTest() {
-        assertEquals(20, 21);
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Logan", "Paul", 1000.00, BankAccount.CHECKING);
+        assertTrue("The account is NOT a checking account", account.isChecking()); // can also be done with assertTrue
     }
 }
